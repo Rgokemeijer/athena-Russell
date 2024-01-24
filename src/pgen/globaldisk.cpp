ca1a3d4f9a9bc07088dd47b04c73323c8127b2e9
Russell Gokemeijer <russellgoke@gmail.com>
2024-01-24 10:33:45 -0500

    Adding new variables to time step function

--- src/pgen/globaldisk.cpp
+++ src/pgen/globaldisk.cpp
@@ -796,8 +796,9 @@ void Outflow_X2(MeshBlock *pmb, Coordinates *pco, AthenaArray<Real> &a, FaceFiel
 
 
 void PseudoNewtonian(MeshBlock *pmb, const Real time, const Real dt,
-  const AthenaArray<Real> &prim, 
-  const AthenaArray<Real> &bcc, AthenaArray<Real> &cons)
+  const AthenaArray<Real> &prim, const AthenaArray<Real> &prim_scalar,
+  const AthenaArray<Real> &bcc, AthenaArray<Real> &cons,
+   AthenaArray<Real> &cons_scalar)
 {
 
   AthenaArray<Real> &x1flux=pmb->phydro->flux[X1DIR];
